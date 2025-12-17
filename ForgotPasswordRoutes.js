8e15b60804e158d429e5779440dab367fe8db543
adhithya2525 <jaiadhithyak@gmail.com>
2025-12-17 20:38:07 +0530

    Delete ForgotPasswordRoutes.js

--- ForgotPasswordRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyEmail, resetPassword } = require('../Controllers/ForgotPasswordController');
-
-router.post('/verify-email', verifyEmail);
-router.post('/reset', resetPassword);
-
-module.exports = router;
